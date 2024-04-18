4f893f2130eb8551813057f57be7b3219c8b5f19
DESKTOP-ACDD6TV\User <guewoljohannkionghat@gmail.com>
2024-04-18 21:09:06 +0200

    configuration du mailer local

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController extends AbstractController
 {
